ac32d7748dbfcd75f078d7d7913322611be0e996
mike <mtomlin5@jh.edu>
2021-04-08 17:16:58 -0400

    Added main c file

---
+++ Mike/C/bss.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "data.h"
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   printf("%f\n", de00[0]);
+   return 0;
+}
