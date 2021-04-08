61206e56b1b01e0c8a7030525bb3ffb49fafc1c9
mike <mtomlin5@jh.edu>
2021-04-08 17:50:45 -0400

    worked on c files

--- Mike/C/bss.c
+++ Mike/C/bss.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 #include "data.h"
+#include <math.h>
+
+float f(float x){
+	return x*x*x;
+}
+
+float g(float x){
+	return tanh((double) x);
+}
+
+float mat_sub()
 
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   printf("%f\n", de00[0]);
+   float mu = 5e-6;
+
+   float C[3][3] = {
+    {  0, 0.1, 0.1},
+    {0.1,   0, 0.1},
+    {0.1, 0.1, 0  }
+   };
+
+   float dC[3][3] = {
+    {  0, 0.1, 0.1},
+    {0.1,   0, 0.1},
+    {0.1, 0.1, 0  }
+   };
+
+   float I[3][3] = {
+    {  1, 0, 0},
+    {0,   1, 0},
+    {0, 0, 1}
+   };
+
+
+
+   float S[1][3] = {0,0,0};
+
+   for (int i = 0; i < N; ++i)
+   {
+   	
+   }
+
+
+
+
+
    return 0;
 }
